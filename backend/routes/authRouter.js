9ce10c307428632a99ff2f53f9a64a14900703e3
toblan9991 <oluwatobiodulana@gmail.com>
2024-07-22 06:03:59 -0700

    giturl11

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -5,7 +5,22 @@ const { githubAuth, githubAuthCallback, githubAuthRedirect } = require('../contr
 
 const authRouter = express.Router();
 
-authRouter.get('/auth/github', githubAuth);
-authRouter.get('/auth/github/callback', githubAuthCallback, githubAuthRedirect);
+// authRouter.get('/auth/github', githubAuth);
+// authRouter.get('/auth/github/callback', githubAuthCallback, githubAuthRedirect);
+
+authRouter.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
+  next();
+});
+
+authRouter.get('/auth/github', (req, res, next) => {
+  console.log('GET /auth/github triggered');
+  next();
+}, githubAuth);
+
+authRouter.get('/auth/github/callback', (req, res, next) => {
+  console.log('GET /auth/github/callback triggered');
+  next();
+}, githubAuthCallback, githubAuthRedirect);
 
 module.exports = authRouter;
