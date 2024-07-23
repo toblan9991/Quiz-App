bbc0b81cbde3e27bcb3eced41624aafed6998070
toblan9991 <oluwatobiodulana@gmail.com>
2024-07-22 20:57:25 -0700

    giturl20

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(session({
   secret: 'your_secret_key',
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: true } // Set to true if using HTTPS
+  cookie: { secure: false } // Set to true if using HTTPS
 }));
 
 app.use(passport.initialize());
