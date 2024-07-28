0593d85a0865befe7a3865a924117973f128e138
toblan9991 <oluwatobiodulana@gmail.com>
2024-07-28 16:46:38 -0700

    test 5 -1

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -3,8 +3,8 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   // windowMs: 3 * 60 *  1000, // 3 minutes
   windowMs: 5 * 1000, // 5 seconds
-  max: 10,
-  // max: 300, // Limit each IP to 300 requests per windowMs
+  // max: 10,
+  max: 100, // Limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again after a minute',
   headers: true,
   handler: (req, res) => {
